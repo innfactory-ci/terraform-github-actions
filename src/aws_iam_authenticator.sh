081425f4b8ad7907b9ffb4341fde31bdcdcbc1d3
Tobias Jonas <t.jonas@innfactory.de>
2020-05-08 11:01:10 +0200

    Change AWS IAM echo

--- src/aws_iam_authenticator.sh
+++ src/aws_iam_authenticator.sh
@@ -2,11 +2,10 @@
 
 function installAWSIAMAuthenticator {
     echo "Downloading AWS IAM Authenticator"
-    curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/aws-iam-authenticator
+    curl -s -S -L -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/aws-iam-authenticator
     echo "Successfully downloaded AWS IAM Authenticator"
     chmod +x ./aws-iam-authenticator
     mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
     echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
-    echo "I will use the $AWS_PROFILE aws profile."
-    aws sts get-caller-identity
+    echo "I will use the [$AWS_PROFILE] aws profile."
 }
